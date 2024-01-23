bfb105dcb0a0a96647110687a1049da7689e62e1
IBRAHIM KASSOUM Habibou <128358617+KassoumHabibou@users.noreply.github.com>
2024-01-24 00:14:33 +0100

    Updating some codes

---
+++ scripts/01 Wrangling flood data.py
@@ -0,0 +1,10 @@
+
+# Load all packages
+import numpy as np
+import xarray as xr
+import cv2 as cv
+import os
+import pandas as pd
+import gdown
+import src.Utils as utils
+from zipfile import ZipFile 
